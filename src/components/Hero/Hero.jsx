9457671ec0539ab8fe3dc2f399f316f49b2db9e2
Ananthak <Ananthakrishna.K@iiitb.ac.in>
2023-08-04 14:20:24 +0530

    more animiations and styling added

--- src/components/Hero/Hero.jsx
+++ src/components/Hero/Hero.jsx
@@ -2,6 +2,7 @@ import './Hero.css';
 import React from 'react';
 import HeroImage from '../../assets/hero.png'
 import { Link } from 'react-router-dom';
+import { TypeAnimation } from 'react-type-animation';
 
 export default function Hero() {
   return (
@@ -11,16 +12,15 @@ export default function Hero() {
             <img src={HeroImage} alt="Gym-man" />
         </div>
         <div className='text'>
-              <h1>Sweat, Smile </h1>
-              <h1 className='bottom-text'> and Repeat</h1>
-              <p>Explore highly effective exercises without the need for a trainer.</p>
+              <h1><TypeAnimation sequence={["Workout.",2000,"PowerUp.",2000,"Improve.",2000,"Progress.",2000]} speed={5} repeat={Infinity} /></h1>
+              <p>By exploring highly effective exercises without the need for a trainer.</p>
               <div className='explore-button'>
                 <Link className='btn-txt' to='/exercises'>Explore Now   <i class='bx bx-right-arrow' ></i></Link>
               </div>
         </div>    
     </div>
     <div className='subtext'>
-    <span>#WORKOUT</span>
+    <span>#STRONG</span>
   </div>
   </>
   )
